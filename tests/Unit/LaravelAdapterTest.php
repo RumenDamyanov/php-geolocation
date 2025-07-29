2b8f705e7c061076618578617244023d5eb1fb09
Rumen Damyanov <dev@rumenx.com>
2025-07-29 14:56:19 +0300

    Refactor and cleanup

--- tests/Unit/LaravelAdapterTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-describe('Laravel GeolocationMiddleware', function () {
-    // Skipped: Framework not present in dev environment
-    it('skipped Laravel GeolocationMiddleware', function () {
-        $this->markTestSkipped('Laravel not available.');
-    });
-});
--- tests/Unit/SymfonyAdapterTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-describe('Symfony GeolocationListener', function () {
-    // Skipped: Framework not present in dev environment
-    it('skipped Symfony GeolocationListener', function () {
-        $this->markTestSkipped('Symfony not available.');
-    });
-});
